44455e0d5a79355702d7001e13aaf1730f4fe86b
Adam Horton <adamhorton@Adams-MacBook-Pro-2.local>
2022-10-09 18:00:58 -0500

    try to fix prod previews

--- components/_page-containers/preview-page.tsx
+++ components/_page-containers/preview-page.tsx
@@ -6,28 +6,55 @@ interface PreviewPageProps extends PropsWithChildren {
 }
 
 const PreviewPage: React.FC<PreviewPageProps> = ({ children, document }) => {
+  const parentDoc = window.document;
+  const previewFrameDoc = document;
+  const previewFrameHead = previewFrameDoc.head || previewFrameDoc.getElementsByTagName("head")[0];
 
-  function appendStyleTextToFrameHead (cssText: string) {
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const style = document.createElement('style');
+  function appendStyleTextToFrameHead(cssText: string) {
+    const style = previewFrameDoc.createElement("style");
+    previewFrameHead.appendChild(style);
 
-    head.appendChild(style);
-
-    style.setAttribute('type', 'text/css');
-    style.appendChild(document.createTextNode(cssText));
+    style.setAttribute("type", "text/css");
+    style.appendChild(previewFrameDoc.createTextNode(cssText));
   }
 
   useEffect(() => {
     //-- relocate css module <style> tags into the iframe created by Netlify CMS preview
-    const parentDoc = window.document;
-    const previewHtmlElement = document.querySelector('html');
+    const previewHtmlElement = previewFrameDoc.querySelector("html");
 
-    const styleTexts = Array.prototype.slice.call(parentDoc.querySelectorAll('style'))
-      .filter(styleTag => styleTag.attributes.length === 0) // css-loader style tags don't have any attributes
-      .map(styleTag => styleTag.innerText);
+    /**
+     * NEXT DEV MODE
+     * The React components will drop style tags into the parent document head
+     * netlify uses emotion - don't copy the style tags created by emotion
+     */
+    const styleTexts = Array.prototype.slice // convert nodelist to array
+      .call(parentDoc.querySelectorAll("style:not([data-emotion])"))
+      .map((styleTag) => styleTag.innerText);
 
-    styleTexts.forEach(cssText => appendStyleTextToFrameHead(cssText));
-    previewHtmlElement!.setAttribute('preview-container', '');
+    styleTexts.forEach((cssText) => appendStyleTextToFrameHead(cssText));
+    previewHtmlElement!.setAttribute("preview-container", "");
+
+    /**
+      * NEXT PROD BUILD
+      * In prod mode next produces CSS file bundles with cache busting style file names,
+      * but they seem to use the '_next/static/css' directory predictably.
+      *
+      * Try to carefully pick those link tags out to duplicate into the preview frame.
+      */
+    const frameLinkTagCopies = Array.prototype.slice
+      .call(parentDoc.querySelectorAll("link[href*='_next'][rel='stylesheet']"))
+      .map((parentLinkEle: HTMLLinkElement) => {
+        /**
+          * I thought about using node.cloneNode() here,
+          * but I'm not sure that's great if we are relocating a clone from one document to another.
+          * ~Adam
+          */
+         const linkEleCopy = previewFrameDoc.createElement('link');
+         previewFrameHead.appendChild(linkEleCopy);
+
+         linkEleCopy.setAttribute('rel', parentLinkEle.getAttribute('rel')||'');
+         linkEleCopy.setAttribute('href', parentLinkEle.getAttribute('href')||'');
+      });
   }, []);
 
   return (
